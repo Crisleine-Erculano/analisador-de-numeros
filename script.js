5088e066963635d6869673619080c842df5b5f96
Crisleine Erculano <candidocrislaine576@gmail.com>
2023-02-16 13:36:26 -0300

    Criando Função do botão finalizar

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var numero = document.getElementById('number');
 var select = document.getElementById('selectValores');
 var resultado = document.getElementById('resultado');
 var valores = [];
+
 function verificar(n){
     if(Number(n)>=1 && Number(n)<= 100){
         return true;
@@ -11,26 +12,52 @@ function verificar(n){
 
 }
 function listar(n,lis){
-    if(lis.indexOf(Number(n) )!= -1){
+    if(lis.indexOf(Number(n))!= -1){
         return true;
     }else{
         return false;
     }
 }
-  function adicionar() {
-    if(verificar(numero.value)&& !listar(numero.value, valores)){
-        valores.push();
+function adicionar() {
+    if(verificar(numero.value) && !listar( numero.value, valores)){
+        valores.push(Number(numero.value));
         let opitions = document.createElement('Option');
         opitions.text = `Valor ${numero.value} adicionado.`;
         select.appendChild(opitions);
-        res.innerHTML = '';
+        resultado.innerHTML = '';
      } else {
-        window.alert("valor inválido ou já consta.");
+        alert("valor inválido ou já constado na lista.");
      }
     numero.value = '';
     numero.focus();
+}
+function finalizar() {
+    if (valores.length == 0){
+        alert('Por favor digite um número de 1 a 100');
+    }else{
+        let soma = 0;
+        let maiorValor = Math.max(...valores);
+        let menorValor = Math.min(...valores);
+        
+        for (let pos in valores) {
+            soma += valores[pos];
+        }
+        mediaNumeros = soma / valores.length;
+        resultado.innerHTML +=`<p>O total de números cadastrados é: ${valores.length}</p>` ;
+        resultado.innerHTML += `<p>A soma dos valores é: ${soma}</p>`;
+        resultado.innerHTML +=`<p>O maior valor encontrado é o número: ${maiorValor}</p>`;      
+        resultado.innerHTML +=`<p>O menor valor encontrado é o número: ${menorValor}</p>`;  
+        resultado.innerHTML +=`<p>a média calculada é: ${mediaNumeros}</p>` ;
+       
+
     }
     
+   
+    
+    
+}
+   
 
+   
 
-
+ 
